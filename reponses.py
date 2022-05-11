cc3d7cbad30843234318e67cb210f06fb74f1c86
Alexis CLEMENT <clemalex20@gmail.com>
2022-05-12 00:06:46 +0200

    Create reponses.py

---
+++ reponses.py
@@ -0,0 +1,17 @@
+# Réponses
+
+## For
+
+```python
+fn0 = 0
+fn1 = 1
+fn2 = fn1 + fn0
+
+compteur = 0
+
+while fn2 < 80000000000:
+    fn2 = fn2 + fn1
+    fn1 = fn2
+    compteur = compteur + 1
+print(compteur)
+```
